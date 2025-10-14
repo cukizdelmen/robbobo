a5f3b609c628a8b35e9fccc783e7254c2f4a5aad
cukizdelmen <thehermans.c@gmail.com>
2025-10-14 03:35:11 -0700

    First Phase of the admin dashboard, awaiting improvemnt

--- src/Config/supabseClient.ts
+++ src/Config/supabseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://brywwnmpucfnwgoppkqo.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyeXd3bm1wdWNmbndnb3Bwa3FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzMzM3OTAsImV4cCI6MjA2OTkwOTc5MH0.6gNXNYouNia_MBxUMsxZRLNKBQpI0xIeNbLarJd8KCE";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
